a1498e5854b83e3ebe138980f8790e7a2222b9b6
ultimatelype <ultimatelype98@gmail.com>
2022-11-09 15:55:13 +0000

    add file for styling using MUI

---
+++ src/mui-style.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from 'tss-react/mui';
+
+export const useStyles = makeStyles()(() => ({
+    shortBtn: {
+        marginTop: '1.7em',
+    },
+    marginContainer: {
+        margin: '2%',
+        alignItems: 'left',
+    },
+}));
